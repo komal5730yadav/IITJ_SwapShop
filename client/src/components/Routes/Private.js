261f7d2f54647bf2b7b06f7203c77229bceaf547
komal5730yadav <komalcute701@gmail.com>
2023-09-26 03:45:52 +0530

    previous_condn

--- client/src/components/Routes/Private.js
+++ client/src/components/Routes/Private.js
@@ -1,24 +1,30 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { useAuth } from "../../context/auth";
 import { Outlet } from "react-router-dom";
 import axios from "axios";
 import Spinner from "../Spinner";
 
 export default function PrivateRoute() {
-  const [ok, setOk] = useState(false);
   const [auth, setAuth] = useAuth();
 
   useEffect(() => {
     const authCheck = async () => {
-      const res = await axios.get("/api/v1/auth/user-auth");
-      if (res.data.ok) {
-        setOk(true);
-      } else {
-        setOk(false);
+      try {
+        const res = await axios.get("/api/v1/auth/user-auth");
+        if (!res.data.ok) {
+          // If the user is not authenticated, you can redirect them to a login page
+          // For example, you can use React Router's `useNavigate` to navigate to a login page
+          // Or you can set a state variable to handle unauthorized access
+        }
+      } catch (error) {
+        // Handle any errors that occur during the request
+        console.error(error);
       }
     };
+
     if (auth?.token) authCheck();
   }, [auth?.token]);
 
-  return ok ? <Outlet /> : <Spinner />;
+  // Render the Outlet directly when the user is authenticated
+  return auth?.token ? <Outlet /> : <Spinner />;
 }
--- client/src/context/auth.js
+++ client/src/context/auth.js
@@ -2,27 +2,33 @@ import { useState, useEffect, useContext, createContext } from "react";
 import axios from "axios";
 
 const AuthContext = createContext();
+
 const AuthProvider = ({ children }) => {
   const [auth, setAuth] = useState({
     user: null,
     token: "",
   });
 
-  //default axios
-  axios.defaults.headers.common["Authorization"] = auth?.token;
-
   useEffect(() => {
     const data = localStorage.getItem("auth");
     if (data) {
       const parseData = JSON.parse(data);
       setAuth({
-        ...auth,
         user: parseData.user,
         token: parseData.token,
       });
+      axios.defaults.headers.common["Authorization"] = parseData.token;
+    } else {
+      // If there's no token in local storage, clear the headers and auth state
+      delete axios.defaults.headers.common["Authorization"];
+      setAuth({
+        user: null,
+        token: "",
+      });
     }
-    //eslint-disable-next-line
+    // eslint-disable-next-line
   }, []);
+
   return (
     <AuthContext.Provider value={[auth, setAuth]}>
       {children}
@@ -30,7 +36,6 @@ const AuthProvider = ({ children }) => {
   );
 };
 
-// custom hook
 const useAuth = () => useContext(AuthContext);
 
 export { useAuth, AuthProvider };
